83f7c6e90122b47f213b1e5f4c8511b42f503715
Bill Centinaro <billc@seatengine.com>
2016-06-28 09:53:04 -0400

    Updated version, and changed maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "elixir"
-maintainer       "Jamie Winsor"
-maintainer_email "jamie@vialstudios.com"
+maintainer       "Bill Centinaro"
+maintainer_email "bill@centinaro.net"
 license          "Apache 2.0"
 description      "Manages an Elixir installation"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.11.0"
+version          "0.12.0"
 
 depends "apt", "~> 2.7"
 depends "git"
